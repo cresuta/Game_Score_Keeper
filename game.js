363961d7d60b26b5b1eb4570d61b336fe8b192d8
Cameron Resuta <60229891+cresuta@users.noreply.github.com>
2021-02-08 15:03:37 -0500

    refactoring variable names, testing game functionality with winning score value

--- game.js
+++ game.js
@@ -1,25 +1,40 @@
-const playerOneBtn = document.querySelector('.playerOneBtn');
-const playerOneScore = document.querySelector('#playerOneScore');
-const playerTwoBtn = document.querySelector('.playerTwoBtn');
-const playerTwoScore = document.querySelector('#playerTwoScore');
-const resetBtn = document.querySelector('.resetBtn')
+const p1Btn = document.querySelector('.p1Btn');
+const p1Display = document.querySelector('#p1Display');
+const p2Btn = document.querySelector('.p2Btn');
+const p2Display = document.querySelector('#p2Display');
+const resetBtn = document.querySelector('.resetBtn');
+const options = document.querySelectorAll('option')
 
-let playerOneScoreTotal = 0;
-let playerTwoScoreTotal = 0;
+let p1Score = 0;
+let p2Score = 0;
 
-playerOneBtn.addEventListener('click', () => {
-    playerOneScoreTotal++;
-    playerOneScore.innerText = playerOneScoreTotal;
+let winningScore = 5;
+let isGameOver = false;
+
+p1Btn.addEventListener('click', () => {
+    if(!isGameOver) {
+        p1Score++;
+        if(p1Score === winningScore) {
+           isGameOver = true;
+        }
+        p1Display.innerText = p1Score;
+    }
+    
 })
 
-playerTwoBtn.addEventListener('click', () => {
-    playerTwoScoreTotal++;
-    playerTwoScore.innerText = playerTwoScoreTotal;
+p2Btn.addEventListener('click', () => {
+    if(!isGameOver) {
+        p2Score++;
+        if(p2Score === winningScore) {
+           isGameOver = true;
+        }
+        p2Display.innerText = p2Score;
+    }
 })
 
 resetBtn.addEventListener('click', () => {
-    playerOneScoreTotal = 0;
-    playerOneScore.innerText = playerOneScoreTotal;
-    playerTwoScoreTotal = 0;
-    playerTwoScore.innerText = playerTwoScoreTotal;
+    p1Score = 0;
+    p1Display.innerText = p1Score;
+    p2Score = 0;
+    p2Display.innerText = p2Score;
 })
